d7c6838c97f4c8c109fb3f8771cc9077320fd842
Peter Strömberg <pez@pezius.com>
2022-01-30 08:58:43 +0100

    WIP

--- PrimeClojure/solution_3/run.sh
+++ PrimeClojure/solution_3/run.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
-clojure -X sieve/run :variant :vector :warm-up? false
-clojure -X sieve/run :variant :vector-transient :warm-up? false
+#clojure -X sieve/run :variant :vector :warm-up? false
+#clojure -X sieve/run :variant :vector-transient :warm-up? false
 clojure -X sieve/run :variant :bitset :warm-up? true
 #clojure -X sieve/run :variant :bitset-all :warm-up? false
 #clojure -X sieve/run :variant :bitset-pre :warm-up? false
